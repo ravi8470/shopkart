18214e19c12f402884aebfc1b5fdf693efb55e71
ravi <ravi.kumar@dreamztech.com>
2019-09-13 19:55:36 +0530

    login related changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
-
+  isLoggedIn: boolean;
+  categoryName: string;
+  constructor(private authService: AuthService, private router: Router) {
+    this.isLoggedIn = !!this.authService.currentUserValue;
+  }
+  goToCategory() {
+    this.router.navigate([`category/${this.categoryName}`])
+  }
   ngOnInit() {
+    this.authService.currentUser.subscribe(data => {
+      this.isLoggedIn = data ? true : false;
+    })
+  }
+
+  logout() {
+    this.authService.logout();
   }
 
 }
