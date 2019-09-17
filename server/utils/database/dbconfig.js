6cdc7a1caf0fbe1933e36b21544cbe0686119cad
ravi <ravi.kumar@dreamztech.com>
2019-09-17 14:28:14 +0530

    added db logging

--- server/utils/database/dbconfig.js
+++ server/utils/database/dbconfig.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+const debug = require("debug")("express-mongoose-es6-rest-api:index");
 require("dotenv").config();
 
 export const connectDB = () => {
+  mongoose.set("debug", (collectionName, method, query, doc) => {
+    debug(`${collectionName}.${method}`, util.inspect(query, false, 20), doc);
+  });
   mongoose.connect(
     `mongodb+srv://${process.env.MONGO_ATLAS_USERNAME}:${process.env.MONGO_ATLAS_PASS}@ravi-mongo-cluster-v9xqc.mongodb.net/shopkart?retryWrites=true&w=majority`,
     { useNewUrlParser: true }
