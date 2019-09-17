79ce325d7fac61169be05f1930b81e3e4f38321f
ravi <ravi.kumar@dreamztech.com>
2019-09-17 17:47:27 +0530

    delete all UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
 import { ProductdetailComponent } from './productdetail/productdetail.component';
 import { PaymentComponent } from './payment/payment.component';
-import { LoginComponent } from './login/login.component';
-import { CategoryPageComponent } from './category-page/category-page.component';
 import { NotfoundComponent } from "./notfound/notfound.component";
 
 const routes: Routes = [
@@ -14,24 +11,12 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   {
-    path: 'home',
-    component: HomepageComponent
-  },
-  {
     path: 'payment',
     component: PaymentComponent
   },
   {
     path: 'productdetails',
     component: ProductdetailComponent
-  },
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'category/:name',
-    component: CategoryPageComponent
   },
   {
     path: '**',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CarouselComponent } from './carousel/carousel.component';
-import { FooterComponent } from './footer/footer.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HomepagebodyComponent } from './homepagebody/homepagebody.component';
-import { HomepageComponent } from './homepage/homepage.component';
 import { ProductdetailComponent } from './productdetail/productdetail.component';
 import { PaymentComponent } from './payment/payment.component';
-import { LoginComponent } from './login/login.component';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { CategoryPageComponent } from './category-page/category-page.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    CarouselComponent,
-    FooterComponent,
-    SidebarComponent,
-    HomepagebodyComponent,
-    HomepageComponent,
     ProductdetailComponent,
     PaymentComponent,
-    LoginComponent,
-    CategoryPageComponent,
     NotfoundComponent
   ],
   imports: [
@@ -36,7 +20,7 @@ import { NotfoundComponent } from './notfound/notfound.component';
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
