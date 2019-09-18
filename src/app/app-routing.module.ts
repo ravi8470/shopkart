5b7ca5c862e459968d367cc9723418c6dafedf27
ravi <ravi.kumar@dreamztech.com>
2019-09-18 11:42:44 +0530

    added home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductdetailComponent } from './productdetail/productdetail.component';
 import { PaymentComponent } from './payment/payment.component';
 import { NotfoundComponent } from "./notfound/notfound.component";
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   },
   {
     path: 'payment',
